d798d7895d0c0fd2c00be0168a2e89748b7bf9ca
Matheus <nolovedeepmath@protonmail.com>
2020-03-05 15:11:34 -0300

    css consert

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,11 +1,7 @@
-from flask import render_template
+from flask import Flask, render_template, url_for, redirect
 from app import app
 
-@app.route("/home")
+@app.route("/index")
 @app.route("/")
 def index():
     return render_template('index.html')
-
-@app.route("/base")
-def login():
-    return render_template('base.html')
