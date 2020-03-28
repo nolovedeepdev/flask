c6062d9c52754272f37370200dbb1f622f4f28e9
nolovedeepmath <nolovedeepdev@debian.matheus>
2020-03-28 00:29:50 -0300

    ajax update

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,36 +1,34 @@
 from flask import Flask, render_template, url_for, redirect
 from app import app
 
-@app.route("/index")
+@app.route("/index",methods= ['POST','GET'])
 @app.route("/")
-
 def index():
     return render_template('index.html')
 
-@app.route("/bar")
+@app.route('/api',methods= ['POST','GET'])
+def process():
+    try:
+        return render_template('api.html')
+    except expression as identifier:
+        return jsonify({'error' : 'Missing data!'})
+
+@app.route("/bar",methods= ['POST','GET'])
 def bar():
     return render_template('gra-bar.html')
 
-@app.route("/pie")
+@app.route("/pie",methods= ['POST','GET'])
 def pie():
     return render_template('gra-pie.html')
 
-@app.route("/scatter")
+@app.route("/scatter",methods= ['POST','GET'])
 def scatter():
     return render_template('gra-scatterplot.html')
 
-@app.route("/about")
+@app.route("/about",methods= ['POST','GET'])
 def about():
     return render_template('about.html')
 
-@app.route("/refe")
+@app.route("/refe",methods= ['POST','GET'])
 def refe():
     return render_template("/refe.html")
-
-@app.route("/series")
-def series():
-    return render_template("/series.html")
-    
-@app.route("/linguagens")
-def ling():
-    return render_template("/linguagens.html")
