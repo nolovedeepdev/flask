4bc5693b710ab000796922758607061d75232472
nolovedeepmath <nolovedeepdev@debian.matheus>
2020-03-31 20:51:05 -0300

    att

--- app/static/js/test00.js
+++ app/static/js/test00.js
@@ -1,44 +1,135 @@
-function load_page(Document){
-	var xhttp;
-	if(Document){
-		$.ajax({
-			type: 'GET',
-			data: Document,
-			url: Document,
-			success: function (data){
-				("#pagina_retorno").html(data);
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6">
+      <div id="columnChart" style="height: 500px; width: 500px;">
+      </div>
+    </div>
+  </div>
+</div>
 
-			},
-			error: function(error){
-				console.log(error);
-			}
-
-		});
-	};
-	xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("#pagina_retorno").innerHTML = this.responseText;
-	    	}
-		};
-	xhttp.open("GET", "index.html"+str, true);
-	xhttp.send();
-
-}
-
- $(function() {
-//twitter bootstrap script
- 	$("#pagina_retorno").click(function(){
-        $.ajax({
-     		type: "POST",
- 			url: "index",
- 			data: $('arquivo').html(),
-        	success: function(msg){
-                alert(msg);
-         	},
- 			error: function(){
- 			alert("failure");
- 			}
-       });
- });
-});
+// code
+var pieChartValues = [{
+	y: 39.16,
+	exploded: true,
+	indexLabel: "Hello",
+	color: "#1f77b4"
+  }, {
+	y: 21.8,
+	indexLabel: "Hi",
+	color: "#ff7f0e"
+  }, {
+	y: 21.45,
+	indexLabel: "pk",
+	color: " #ffbb78"
+  }, {
+	y: 5.56,
+	indexLabel: "one",
+	color: "#d62728"
+  }, {
+	y: 5.38,
+	indexLabel: "two",
+	color: "#98df8a"
+  }, {
+	y: 3.73,
+	indexLabel: "three",
+	color: "#bcbd22"
+  }, {
+	y: 2.92,
+	indexLabel: "four",
+	color: "#f7b6d2"
+  }];
+  renderPieChart(pieChartValues);
+  
+  function renderPieChart(values) {
+  
+	var chart = new CanvasJS.Chart("pieChart", {
+	  backgroundColor: "white",
+	  colorSet: "colorSet2",
+  
+	  title: {
+		text: "Pie Chart",
+		fontFamily: "Verdana",
+		fontSize: 25,
+		fontWeight: "normal",
+	  },
+	  animationEnabled: true,
+	  data: [{
+		indexLabelFontSize: 15,
+		indexLabelFontFamily: "Monospace",
+		indexLabelFontColor: "darkgrey",
+		indexLabelLineColor: "darkgrey",
+		indexLabelPlacement: "outside",
+		type: "pie",
+		showInLegend: false,
+		toolTipContent: "<strong>#percent%</strong>",
+		dataPoints: values
+	  }]
+	});
+	chart.render();
+  }
+  var columnChartValues = [{
+	y: 686.04,
+	label: "one",
+	color: "#1f77b4"
+  }, {
+	y: 381.84,
+	label: "two",
+	color: "#ff7f0e"
+  }, {
+	y: 375.76,
+	label: "three",
+	color: " #ffbb78"
+  }, {
+	y: 97.48,
+	label: "four",
+	color: "#d62728"
+  }, {
+	y: 94.2,
+	label: "five",
+	color: "#98df8a"
+  }, {
+	y: 65.28,
+	label: "Hi",
+	color: "#bcbd22"
+  }, {
+	y: 51.2,
+	label: "Hello",
+	color: "#f7b6d2"
+  }];
+  renderColumnChart(columnChartValues);
+  
+  function renderColumnChart(values) {
+  
+	var chart = new CanvasJS.Chart("columnChart", {
+	  backgroundColor: "white",
+	  colorSet: "colorSet3",
+	  title: {
+		text: "Column Chart",
+		fontFamily: "Verdana",
+		fontSize: 25,
+		fontWeight: "normal",
+	  },
+	  animationEnabled: true,
+	  legend: {
+		verticalAlign: "bottom",
+		horizontalAlign: "center"
+	  },
+	  theme: "theme2",
+	  data: [
+  
+		{
+		  indexLabelFontSize: 15,
+		  indexLabelFontFamily: "Monospace",
+		  indexLabelFontColor: "darkgrey",
+		  indexLabelLineColor: "darkgrey",
+		  indexLabelPlacement: "outside",
+		  type: "column",
+		  showInLegend: false,
+		  legendMarkerColor: "grey",
+		  dataPoints: values
+		}
+	  ]
+	});
+  
+	chart.render();
+  }
