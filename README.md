502c5cc373f8a7666ff015dbe6df74d178cd0fb4
nolovedeepdev <nolovedeepmath@protonmail.com>
2020-06-23 15:17:06 -0300

    readme update

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 Intitulado Proposta e Avaliação de Templates de descrição Textual para vocalização de gráficos de barras, este projeto tem como objetivo criar uma aplicação web a qual visa extrair dados das imagens anexadas(upload) a aplicação web.
 
 ## Preview
+##### Index page
 ![Image description](print.png)
 
+##### Interact page
+![Image description](print2.png)
 ## Download and Installation
 * To begin using this application, install all of the following commads to get started:
 * [Download Python 3.8.3](https://www.python.org/ftp/python/3.8.3/python-3.8.3.exe)
@@ -17,9 +20,11 @@ Intitulado Proposta e Avaliação de Templates de descrição Textual para vocal
 ## Usage
 
 ### Basic
-Após a instalação da linguagem Python(3.8) e do framework Flask, estruturar 
+Após as instalações, citadas posteriormente, execute o comando para executar a aplicação
 > python run.py
 
+### Mapa das páginas
+A página Index é direcionada a apresentação do projeto, seu github e devidas apresentações a universidade e laboratório 
 ## Bugs and Issues
 
 ## About
@@ -27,6 +32,7 @@ Após a instalação da linguagem Python(3.8) e do framework Flask, estruturar
 Matheus Antônio Castro
 
 ## Copyright and License
-Universidade Federal do Pará
-Laboratório Visualização, Interação e Sistemas Inteligentes.
-The MIT License (MIT) - Copyright (c) 2013-2019 Blackrock Digital LLC
+* Universidade Federal do Pará
+* Laboratório Visualização, Interação e Sistemas Inteligentes.
+* Laboratório Visualização, Interação e Sistemas Inteligentes.
+* The MIT License (MIT) - Copyright (c) 2013-2019 Blackrock Digital LLC
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,46 +1,85 @@
 from flask import Flask, render_template, url_for, redirect, json, request, Response, jsonify
 from app import app
+import cv2
 from datetime import datetime
 import os
 import jinja2
-import cv2
+
 
 # route from index's page
 @app.route('/')
-@app.route('/index', methods= ['POST', 'GET'])
+@app.route('/index', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
-# route from bar's page
-@app.route("/bar", methods= ['POST','GET'])
-def bar():
-    return render_template('gra-bar.html')
-
-# route from pie's page
-@app.route("/pie", methods= ['POST','GET'])
-def pie():
-    return render_template('gra-pie.html')
-
-# route from scatter's page
-@app.route("/scatter", methods= ['POST','GET'])
-def scatter():
-    return render_template('gra-scatterplot.html')
 
 # route from interaction's page
-@app.route("/interact/", methods= ['POST','GET'])
+@app.route("/interact/", methods=['POST', 'GET'])
 def inte():
     return render_template('interact.html')
 
-@app.route("/interact/axis", methods= ['POST','GET'])
-def main(): # fuction that returns the axes
-    obj_img = cv2.imread(r'app/static/img/bar0.png')
-    altura, largura, cores = obj_img.shape
 
-    for y in range(0, altura): # laço que pega os eixos
-        for x in range(0, largura):
-            x = str(x); y = str(y) # transforma as variaveis em string p/ não haver possíveis erros
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
 
-    return 'x={}, y={}; '.format(x, y) # return que aparece no front-end
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/interact/axis", methods=['POST', 'GET'])
+def mouseClick():
+    outputFrame = None
+    img = cv2.imread(r'app/static/img/bar0.png')  # Carregar a imagem do terminal p/ o opencv
+    clicked = False  # Declarar a variável global
+    xpos = ypos = 0  # xpos = posição x, ypos = posição y
+    axislist = []  # List to take the axis positions
+
+    def generate(img):
+        # grab global references to the output frame and lock variables
+        global outputFrame, lock
+
+        # loop over frames from the output stream
+        while True:
+            # wait until the lock is acquired
+            with lock:
+                # check if the output frame is available, otherwise skip
+                # the iteration of the loop
+                if outputFrame is None:
+                    continue
+
+                # encode the frame in JPEG format
+                (flag, encodedImage) = cv2.imshow("image", img)
+
+                # ensure the frame was successfully encoded
+                if not flag:
+                    continue
+
+            # yield the output frame in the byte format
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                   bytearray(encodedImage) + b'\r\n')
+
+
+    def takeAxis(event, x, y, flags, param):  # Função para pegar as cordenadas x e y com o mouse
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            global xpos, ypos, clicked
+            clicked = True
+            axislist.append(y)
+            axislist.append(x)
+
+    cv2.namedWindow("image")
+    cv2.setMouseCallback('image', takeAxis)  # Comando que permite a comunicação/ recebimento dos clicks
+
+    while 1:  # Laço que permite um proximo click para resgatar o eixo
+        generate()
+
+        if clicked:  # Condicional para captura o eixo em uma lista
+            clicked = False
+
+        if cv2.waitKey(20) & 0xFF == 27:  # Condicional para parar o laço ao teclar Esc
+            break
+
+    return 'cuh', axislist
+
+
+@app.route("/image_output", methods=['POST', 'GET'])
+def image_output():
+    return Response(mouseClick())
